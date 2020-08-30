77b2ad3da512e67395e5d098fb5e44210545c341
unknown <wangxiaoyan1@xiaomi.com>
2020-08-30 16:16:51 +0800

    gsondemo commit

---
+++ app/src/main/java/com/example/gsondemo/GsonDemo.java
@@ -0,0 +1,30 @@
+package com.example.gsondemo;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangxiaoyan on 2020/8/29.
+ */
+public class GsonDemo {
+    private static final String TAG = "GsonDemo";
+    private String name = "";
+    private String hobby = "";
+    private Map map = new HashMap();
+
+    public GsonDemo(String name, String hobby, Map map) {
+        this.name = name;
+        this.hobby = hobby;
+        this.map = map;
+    }
+
+    @Override
+    public String toString() {
+        return "GsonDemo{" +
+                "name='" + name + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/gsondemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.gsondemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Map map = new HashMap();
+        map.put("name", "xiaotian");
+        GsonDemo gsonDemo = new GsonDemo("xiaoai", "sport", map);
+        Gson gson = new Gson();
+        String json = gson.toJson(gsonDemo);
+        Log.i(TAG, "json= " + json);
+
+
+        JsonObject object = new JsonObject();
+        object.addProperty("name", "xiaoai");
+        object.addProperty("hobby", "sport");
+        String s = object.toString();
+        Log.i(TAG, "s= " + s);
+    }
+}
